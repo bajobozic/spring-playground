a0c8c06a2afd6efbac6b3b8712c2fe90192d32e4
bajobozic <bajobozic@gmail.com>
2020-04-24 01:24:22 +0200

    Comparasion refactor

--- src/main/java/com/example/tacocloud/security/providers/UsernamePasswordAuthenticationProvider.java
+++ src/main/java/com/example/tacocloud/security/providers/UsernamePasswordAuthenticationProvider.java
@@ -6,7 +6,6 @@ import com.example.tacocloud.security.services.SecureUserDetails;
 import com.example.tacocloud.security.services.SecureUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,6 +37,6 @@ public class UsernamePasswordAuthenticationProvider implements AuthenticationPro
 
     @Override
     public boolean supports(Class<?> aClass) {
-        return UsernamePasswordAuthentication.class.equals(aClass) || UsernamePasswordAuthenticationToken.class.equals(aClass);
+        return aClass.isAssignableFrom(UsernamePasswordAuthentication.class);
     }
 }
